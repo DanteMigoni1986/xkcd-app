3cfd1e1e60ada2e53681045b42787639f2533cb3
Dante Jose Migoni Leon <dantemigoni@hotmail.com>
2023-04-28 10:17:52 -0600

    adding final updates

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,17 @@
 import { NextUIProvider } from '@nextui-org/react'
 import '@/styles/globals.css'
 import Head from "next/head"
+import { I18NProvider } from '@/context/i18n'
 
 export default function App({ Component, pageProps }) {
   return (
-    <NextUIProvider>
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Component {...pageProps} />
-    </NextUIProvider>
+    <I18NProvider>
+      <NextUIProvider>
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Component {...pageProps} />
+      </NextUIProvider>
+    </I18NProvider>
   )
 }
